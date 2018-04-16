37ba841cbc9bc9db4ee713e7778fa62765ebe025
Electricz1337 <30772723+Electricz1337@users.noreply.github.com>
2018-04-16 08:37:02 +0200

    Create create_run.sh

---
+++ create_run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ""
+echo "PUBG PC IP (192.168.??.??)"
+read game_ip
+
+echo ""
+echo "Router (gateway) IP (192.168.??.??)"
+read router_ip
+
+echo ""
+echo "Radar PC IP (this machine) (192.168.??.??)"
+read radar_ip
+
+echo ""
+echo "interface name"
+ls /sys/class/net
+
+read interface
+
+cat >run.sh <<EOF
+#!/bin/bash
+sysctl -w net.ipv4.ip_forward=1
+arpspoof -i $interface -t $game_ip $router_ip & >/dev/null
+arpspoof -i $interface -t $router_ip $game_ip & >/dev/null
+java -jar Gaydar/target/Gaydar-6.9-jar-with-dependencies.jar $radar_ip PortFilter $game_ip
+EOF
+
+chmod +x run.sh
