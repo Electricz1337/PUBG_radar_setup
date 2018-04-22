13027cbb2559f8522643104801e175f581f5768f
Electricz1337 <30772723+Electricz1337@users.noreply.github.com>
2018-04-22 15:20:59 +0200

    Update create_run.sh

--- create_run.sh
+++ create_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo ""
-echo "PUBG PC IP (192.168.??.??)"
+echo "PUBG PC IP"
 read game_ip
 
 echo ""
-echo "Router (gateway) IP (192.168.??.??)"
+echo "Router (gateway) IP"
 read router_ip
 
 echo ""
-echo "Radar PC IP (this machine) (192.168.??.??)"
+echo "Radar PC IP"
 read radar_ip
 
 echo ""
@@ -25,13 +25,16 @@ read ram
 cat >run.sh <<EOF
 #!/bin/bash
 sysctl -w net.ipv4.ip_forward=1
-if [ "$1" == '--show_arpspoof' ]
+if [ -z "\$1" ]
 then
-  arpspoof -i $interface -t $game_ip $router_ip
-  arpspoof -i $interface -t $router_ip $game_ip
+  arpspoof -i $interface -t $game_ip $router_ip &> /dev/null &
+  arpspoof -i $interface -t $router_ip $game_ip &> /dev/null &
+elif [ "\$1" == "--show_arpspoof" ]
+then
+  arpspoof -i $interface -t $game_ip $router_ip &
+  arpspoof -i $interface -t $router_ip $game_ip &
 else
-  arpspoof -i $interface -t $game_ip $router_ip &> /dev/null
-  arpspoof -i $interface -t $router_ip $game_ip &> /dev/null
+  echo 'Unknown argument: '\$1
 fi
 java -Xmx$ram -jar PUBG-Radar/target/Gaydar-6.9-jar-with-dependencies.jar $radar_ip PortFilter $game_ip
 EOF
